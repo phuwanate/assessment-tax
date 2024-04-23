99920a94c790e9b1de7f71d067f5604063f83009
First.phuwanate <phuwanate.lpk@gmail.com>
2024-04-23 17:07:34 +0700

    feature/story-1 finish

---
+++ deduction/create_table.go
@@ -0,0 +1,26 @@
+package deduction
+
+import (
+	"log"
+	_ "net/http"
+	_ "strconv"
+	_ "github.com/labstack/echo/v4"
+	"github.com/phuwanate/assessment-tax/db"
+	_ "github.com/lib/pq"
+)
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+func InitDeduction() {
+	db := database.DB
+	var err error
+	createTb := `CREATE TABLE IF NOT EXISTS allowance ( id SERIAL PRIMARY KEY, personalDeduction INT);`
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	db.QueryRow("INSERT INTO  allowance (personalDeduction) values ($1)  RETURNING id", 60000)
+}
